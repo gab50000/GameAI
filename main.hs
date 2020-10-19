256e9aabd9c217c35108c75d5e8aa2fb6b59e767
Gabriel Kabbe <gabriel.kabbe@mail.de>
2020-10-19 12:29:25 +0200

    Update

--- main.hs
+++ main.hs
@@ -4,9 +4,9 @@ import Prelude
 data Player = X | O deriving (Eq, Show)
 
 data State = State
-  { board :: Board,
-    moves :: [Move],
-    current_player :: Player
+  { getBoard :: Board,
+    getMoves :: [Move],
+    getCurrentPlayer :: Player
   }
   deriving (Show)
 
@@ -30,23 +30,62 @@ isFull (row : rest) = all (/= Nothing) row && isFull rest
 determineBestMove :: State -> (Move, Score)
 determineBestMove _ = ((0, 0), 0)
 
+chooseBestMove :: State -> [Move] -> Maybe Move -> Move
+chooseBestMove state (mv : moves) Nothing = chooseBestMove state moves (Just mv)
+chooseBestMove state (new_move : moves) (Just old_move)
+  | moves == [] = old_move
+  | new_score > old_score = chooseBestMove state moves (Just new_move)
+  | otherwise = chooseBestMove state moves (Just old_move)
+  where
+    new_score = getScore state new_move
+    old_score = getScore state old_move
+
 getScore :: State -> Move -> Score
 getScore state move
-  | determineWinner new_board == Just (player) = 1
+  | determineWinner new_board == Just (current_player) = 1
+  | isGameOver new_board = 0
+  | otherwise = - (maximum $ map (getScore new_state) new_moves)
   where
-    new_board = doMove (board state) player move
-    player = current_player state
-    other_player = getOpponent player
+    new_board = updateBoard board current_player move
+    current_player = getCurrentPlayer state
+    board = getBoard state
+    new_state = updateState state move
+    new_moves = getMoves new_state
 
 getOpponent :: Player -> Player
 getOpponent X = O
 getOpponent O = X
 
-doMove :: Board -> Player -> Move -> Board
-doMove board player (i, j) = rows_before ++ [row_beginning ++ [Just player] ++ row_end] ++ rows_after
+getPossibleMoves :: Board -> [Move] -> (Int, Int) -> [Move]
+getPossibleMoves board moves (i, j)
+  | board == [] = moves
+  | row == [] = getPossibleMoves rest_of_board moves (i + 1, 0)
+  | val == Nothing = getPossibleMoves (rest_of_row : rest_of_board) ((i, j) : moves) (i, j + 1)
+  | otherwise = getPossibleMoves (rest_of_row : rest_of_board) moves (i, j + 1)
+  where
+    val : rest_of_row = row
+    row : rest_of_board = board
+
+updateState :: State -> Move -> State
+updateState state move =
+  State
+    { getBoard = new_board,
+      getMoves = getPossibleMoves new_board [] (0, 0),
+      getCurrentPlayer = getOpponent player
+    }
+  where
+    board = getBoard state
+    new_board = updateBoard board player move
+    player = getCurrentPlayer state
+
+updateBoard :: Board -> Player -> Move -> Board
+updateBoard board player (i, j) = rows_before ++ [row_beginning ++ [Just player] ++ row_end] ++ rows_after
   where
     (rows_before, row : rows_after) = splitAt i board
     (row_beginning, _ : row_end) = splitAt j row
+
+isGameOver :: Board -> Bool
+isGameOver board = getPossibleMoves board [] (0, 0) == []
 
 determineWinner :: Board -> Maybe Player
 determineWinner board
@@ -73,6 +112,43 @@ diagonal :: Board -> [Maybe Player]
 diagonal [] = []
 diagonal (row : rest) = head row : diagonal rest
 
+empty_board :: Board
+empty_board = take 3 $ repeat $ take 3 $ repeat Nothing
+
+start_state :: State
+start_state =
+  State
+    { getBoard = empty_board,
+      getMoves = getPossibleMoves empty_board [] (0, 0),
+      getCurrentPlayer = X
+    }
+
+playAgainstHuman :: State -> IO ()
+playAgainstHuman state = do
+  printBoard $ getBoard state
+  print $ "Your turn"
+  move <- getInput
+  let new_state = updateState state move
+  print $ "Calculating best move..."
+  let best_move = chooseBestMove new_state (getMoves new_state) Nothing
+  let ai_state = updateState new_state best_move
+  playAgainstHuman ai_state
+
+printBoard :: Board -> IO ()
+printBoard (row : rest)
+  | rest /= [] = print (line_str) >> print (take 20 $ repeat '-') >> printBoard rest
+  | otherwise = print (line_str)
+  where
+    (a : b : c : _) = row
+    line_str = (show a) ++ "|" ++ show b ++ "|" ++ show c
+
+getInput :: IO (Int, Int)
+getInput = do
+  input <- getLine
+  let num = read input - 1
+  let i = div num 3
+  let j = mod num 3
+  return (i, j)
+
 main :: IO ()
-main = forever $ do
-  print X
+main = playAgainstHuman start_state
