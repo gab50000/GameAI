980b1ad1da4cf96ecf1ef601f713c30d2f5712bd
Gabriel Kabbe <gabriel.kabbe@mail.de>
2020-10-22 20:43:42 +0200

    Move implementation of chooseBestMove to type class

--- Game.hs
+++ Game.hs
@@ -6,5 +6,16 @@ type Score = Int
 
 class GameState game where
   type Move game
+
   chooseBestMove :: game -> [Move game] -> Maybe (Move game) -> (Move game)
+  chooseBestMove state (mv : moves) Nothing = chooseBestMove state moves (Just mv)
+  chooseBestMove state moves (Just old_move)
+    | length moves == 0 = old_move
+    | new_score > old_score = chooseBestMove state moves (Just new_move)
+    | otherwise = chooseBestMove state rest_moves (Just old_move)
+    where
+      new_score = getScore state new_move
+      old_score = getScore state old_move
+      (new_move : rest_moves) = moves
+
   getScore :: game -> (Move game) -> Score
--- main.hs
+++ main.hs
@@ -7,16 +7,6 @@ import Prelude
 instance GameState TicTacToeState where
   type Move TicTacToeState = (Int, Int)
 
-  chooseBestMove state (mv : moves) Nothing = chooseBestMove state moves (Just mv)
-  chooseBestMove state moves (Just old_move)
-    | moves == [] = old_move
-    | new_score > old_score = chooseBestMove state moves (Just new_move)
-    | otherwise = chooseBestMove state rest_moves (Just old_move)
-    where
-      new_score = getScore state new_move
-      old_score = getScore state old_move
-      (new_move : rest_moves) = moves
-
   getScore state move
     | determineWinner new_board == Just (current_player) = 1
     | determineWinner new_board == Just (opponent) = -1
