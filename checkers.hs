001ed76ce6d49146d8dc34de6f76b9b2b08300eb
Gabriel Kabbe <gabriel.kabbe@mail.de>
2020-10-25 17:43:56 +0100

    Update checkers

--- checkers.hs
+++ checkers.hs
@@ -8,12 +8,45 @@ type From = Position
 
 type To = Position
 
+type Board = [[Maybe Player]]
+
+data Player = Black | White
+
+exampleBoard :: Board
+exampleBoard =
+  (take 2 $ repeat (take 4 $ repeat (Just White)))
+    ++ (take 2 $ repeat (take 4 $ repeat Nothing))
+    ++ (take 2 $ repeat (take 4 $ repeat (Just Black)))
+
+printPlayer :: Maybe Player -> String
+printPlayer Nothing = " "
+printPlayer (Just Black) = "o"
+printPlayer (Just White) = "●"
+
 instance GameState Checkers where
   type Move Checkers = (From, To)
 
+  getScore state move = 5
+
 data Checkers = Checkers
-  { black_men :: [Position],
-    black_kings :: [Position],
-    white_men :: [Position],
-    white_kings :: [Position]
+  { board :: Board,
+    player :: Player
   }
+
+printBoard :: Board -> Int -> IO ()
+printBoard [] _ = return ()
+printBoard (row : rest) i = do
+  printRow row i
+  printBoard rest (i + 1)
+
+printRow :: [Maybe Player] -> Int -> IO ()
+printRow [] _ = do
+  putStrLn ""
+  return ()
+printRow (x : xs) i
+  | mod i 2 == 0 = do
+    putStr (printPlayer x ++ "-")
+    printRow xs i
+  | otherwise = do
+    putStr ("-" ++ printPlayer x)
+    printRow xs i
