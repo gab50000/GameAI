b71d122458e1a0f08ba95879ac6c8a7396a657cf
Gabriel Kabbe <gabriel.kabbe@mail.de>
2020-11-01 12:54:00 +0100

    Update

--- checkers.hs
+++ checkers.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE TypeFamilies #-}
 
 import Game
+import Prelude hiding (Either (..))
 
 instance GameState Checkers where
   type Move Checkers = (From, To)
@@ -29,6 +30,12 @@ data Color a = Black a | White a
 data Player
 
 data Direction = Up | Down deriving (Eq)
+
+data Side = Left | Right deriving (Eq)
+
+getOppositeColor :: Color a -> Color a
+getOppositeColor (Black a) = (White a)
+getOppositeColor (White a) = (Black a)
 
 exampleBoard :: Board
 exampleBoard =
@@ -76,29 +83,56 @@ getField :: Board -> Position -> Maybe (Color Piece)
 getField [] _ = Nothing
 getField board (i, j) = (board !! i) !! j
 
-getMovesPiece :: Board -> Position -> Direction -> [Move Checkers]
-getMovesPiece board pos dir = getLeftMove board pos dir ++ getRightMove board pos dir
+getMovesPiece :: Checkers -> Position -> Direction -> [Move Checkers]
+getMovesPiece board_ pos dir = getLeftMove board_ pos dir ++ getRightMove board_ pos dir
 
-getLeftMove :: Board -> Position -> Direction -> [Move Checkers]
-getLeftMove board (i, j) dir
+getLeftMove :: Checkers -> Position -> Direction -> [Move Checkers]
+getLeftMove checkers (i, j) dir
   | dir == Up && j == 0 = []
   | dir == Down && j == 7 = []
-  | dir == Up && board !! (i - 1) !! (j - 1) /= Nothing = []
-  | dir == Down && board !! (i + 1) !! (j + 1) /= Nothing = []
+  | dir == Up && board_ !! (i - 1) !! (j - 1) /= Nothing = []
+  | dir == Down && board_ !! (i + 1) !! (j + 1) /= Nothing = []
   | dir == Up = [((i, j), (i - 1, j - 1))]
   | dir == Down = [((i, j), (i + 1, j + 1))]
+  where
+    board_ = board checkers
 
-getRightMove :: Board -> Position -> Direction -> [Move Checkers]
-getRightMove board (i, j) dir
+getRightMove :: Checkers -> Position -> Direction -> [Move Checkers]
+getRightMove checkers (i, j) dir
   | dir == Up && j == 0 = []
   | dir == Down && j == 7 = []
-  | dir == Up && board !! (i - 1) !! (j + 1) /= Nothing = []
-  | dir == Down && board !! (i + 1) !! (j - 1) /= Nothing = []
+  | dir == Up && board_ !! (i - 1) !! (j + 1) /= Nothing = []
+  | dir == Down && board_ !! (i + 1) !! (j - 1) /= Nothing = []
   | dir == Up = [((i, j), (i - 1, j + 1))]
   | dir == Down = [((i, j), (i + 1, j - 1))]
+  where
+    board_ = board checkers
 
-getLeftJump :: Board -> Position -> Direction -> [Move Checkers]
-getLeftJump board (i, j) dir = []
+getLeftJump :: Checkers -> Position -> Direction -> [Move Checkers]
+getLeftJump checkers (i, j) dir
+  | dir == Up && j == 0 = []
+  | dir == Down && j == 7 = []
+  | dir == Up && board_ !! (i - 1) !! (j - 1) /= Nothing = []
+  | dir == Down && board_ !! (i + 1) !! (j + 1) /= Nothing = []
+  | dir == Up = [((i, j), (i - 1, j - 1))]
+  | dir == Down = [((i, j), (i + 1, j + 1))]
+  where
+    board_ = board checkers
+    player_ = player checkers
+    oppositePlayer = getOppositeColor player_
+
+getDiagonalPosition :: Position -> Direction -> Side -> Maybe (Position)
+getDiagonalPosition (0, _) Up _ = Nothing
+getDiagonalPosition (7, _) Down _ = Nothing
+getDiagonalPosition (_, 0) Up Left = Nothing
+getDiagonalPosition (_, 7) Up Right = Nothing
+getDiagonalPosition (_, 7) Down Left = Nothing
+getDiagonalPosition (_, 0) Down Right = Nothing
+getDiagonalPosition (i, j) Up Left = Just (i - 1, j - 1)
+getDiagonalPosition (i, j) Up Right = Just (i - 1, j + 1)
+getDiagonalPosition (i, j) Down Left = Just (i + 1, j + 1)
+getDiagonalPosition (i, j) Down Right = Just (i + 1, j - 1)
+getDiagonalPosition _ _ _ = Nothing
 
 getRightJump :: Board -> Position -> Direction -> [Move Checkers]
 getRightJump board (i, j) dir = []
