5749763b4f94fe5f4921bb5df55a58297b47bc7d
Gabriel Kabbe <gabriel.kabbe@mail.de>
2020-11-19 21:20:03 +0100

    Remove redundant brackets

--- src/Game.hs
+++ src/Game.hs
@@ -7,7 +7,7 @@ type Score = Int
 class GameState game where
   type Move game
 
-  chooseBestMove :: game -> [Move game] -> Maybe (Move game) -> (Move game)
+  chooseBestMove :: game -> [Move game] -> Maybe (Move game) -> Move game
   chooseBestMove state (mv : moves) Nothing = chooseBestMove state moves (Just mv)
   chooseBestMove state moves (Just old_move)
     | [] <- moves = old_move
